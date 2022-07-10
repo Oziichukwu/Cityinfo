513909b5cc68da4546dd6a82fe475f4550509670
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-07-10 18:24:30 +0100

    files controller

--- Cityinfo.API/Controllers/CitiesController.cs
+++ Cityinfo.API/Controllers/CitiesController.cs
@@ -1,17 +1,21 @@
 ﻿using AutoMapper;
 using Cityinfo.API.Models;
 using Cityinfo.API.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace Cityinfo.API.Controllers
 {
 
     [ApiController]
+    [Authorize]
     [Route("api/cities")]
     public class CitiesController: ControllerBase
     {
         private readonly ICityInfoRepository _cityInfoRepository;
         private readonly IMapper _mapper;
+        const int maxCitiesPageSize = 20;
 
         public CitiesController(ICityInfoRepository cityInfoRepository,
             IMapper mapper)
@@ -22,9 +26,19 @@ namespace Cityinfo.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<CityDto>>> GetCities()
-        {
-            var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+        public async Task<ActionResult<IEnumerable<CityDto>>> GetCities(
+            string? name, string? searchQuery, int pageNumber = 1, int pageSize = 10)
+        { 
+            if (pageSize > maxCitiesPageSize)
+            {
+                pageSize = maxCitiesPageSize;
+            }
+            var (cityEntities, paginationMetadata) = await _cityInfoRepository.GetCitiesAsync(name, searchQuery,
+                pageNumber, pageSize);
+
+            Response.Headers.Add("X-Pagination",
+                JsonSerializer.Serialize(paginationMetadata));
+
             var results = _mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities);
             return Ok(results);
 
